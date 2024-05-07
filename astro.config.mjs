e32bcb3f0a3cb35d3a1da25a61ebde31f3ce7022
gagglepod <gagglepod@gmail.com>
2024-05-07 13:56:13 -0500

    NPM Audit update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import NetlifyCMS from "astro-netlify-cms";
+// import NetlifyCMS from "astro-netlify-cms";
 import dcapConfig from "./decap.config.mjs";
 import astropodConfig from "./.astropod/astropod.config.json";
 import robotsTxt from "astro-robots-txt";
@@ -31,8 +31,8 @@ export default defineConfig({
       cacheDir: "./.cache/image",
       logLevel: "debug",
     }),
-    NetlifyCMS({
-      config: dcapConfig(),
-    }),
+    // NetlifyCMS({
+    //   config: dcapConfig(),
+    // }),
   ],
 });
